38d42646720a56db2c56561f8ea2b29029c13460
WHMCS RED <github@whmcs.red>
2017-02-26 14:20:16 -0300

    Create pin.php

---
+++ pin.php
@@ -0,0 +1,50 @@
+<?php
+// Desenvolvido por Joel - WHMCS.RED || Modificações de search inteligente feita por Luciano - WHMCS.RED
+// Pegar Session
+use WHMCS\Session;
+// Pegar Conexão com Banco de Dados
+use WHMCS\Database\Capsule;
+// Bloqueia o acesso direto ao arquivo
+if (!defined("WHMCS")){
+	die("Acesso restrito!");
+}
+// Monta o PIN
+function montar_pin($id){
+	$limite = 6;
+	$montar = md5($id);
+	$montar = preg_replace("/[^0-9]/", "", $montar);
+	$quantidade_numeros = mb_strlen($montar);
+	$contar = $limite - quantidade_numeros;
+	$resultado = substr($montar, $limite, $contar);
+	return $resultado;
+}
+// Página de Administrador
+add_hook("AdminAreaClientSummaryPage", 1, function($vars){
+	return "<div class='alert alert-success'><strong>PIN: ".montar_pin($vars["userid"])."</strong></div>";
+});
+// Página do Cliente
+add_hook("ClientAreaHomepage", 2, function($vars){
+	return "<div class='alert alert-success'><strong>PIN: ".montar_pin($_SESSION["uid"])."</strong></div>";
+});
+// Adicionando função de pesquisa do PIN
+add_hook("IntelligentSearch", 1, function($vars){
+	$pesquisa = array();
+	foreach (Capsule::table("tblclients")->get() as $clientes){
+		$resultado = montar_pin($clientes->id);
+		if($resultado == $vars["searchTerm"]){
+			$idcliente = $clientes->id;
+			$pin = $resultado;
+		}
+	}
+	foreach (Capsule::table("tblclients")->WHERE("id", $idcliente)->get() as $cliente){
+		$pesquisa[] = '
+		<div class="searchresult">
+			<a href="clientssummary.php?userid='.$cliente->id.'">
+				<strong>'.$cliente->firstname.' '.$cliente->lastname.'</strong>
+				(PIN: '.$pin.')<br />
+				<span class="desc">' . $cliente->email . '</span>
+			</a>
+		</div>';
+	}
+	return $pesquisa;
+});
